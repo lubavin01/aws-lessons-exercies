590d847592ee61e7e397016adc87d5ed98137138
Lyubavin Nikolay <lubavin.n@n-t.io>
2022-04-14 11:14:17 +0400

    init

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,3 +1,9 @@
-module.exports.handler = (evt, ctx, done) => {
-  
+module.exports.handler = async (event) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Hello world',
+            input: event,
+        })
+    }
 }
